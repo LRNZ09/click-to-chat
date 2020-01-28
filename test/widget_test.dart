3bb239e9143b50416ff41f05777d870bcd47dba7
Lorenzo Pieri <13508373+LRNZ09@users.noreply.github.com>
2020-01-28 22:55:55 +0100

    Update app widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,23 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mdi/mdi.dart';
 
 import 'package:click_to_chat/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('Home', (WidgetTester tester) async {
     await tester.pumpWidget(App());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    var textField = find.text('Phone number');
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    expect(textField, findsOneWidget);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    var button = find.byIcon(Mdi.whatsapp);
+
+    expect(button, findsOneWidget);
+
+    await tester.tap(button);
+    // await tester.pump();
+
+    // TODO Verify that our button is disabled
   });
 }
