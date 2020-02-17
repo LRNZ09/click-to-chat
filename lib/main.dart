732596d2701ca97fcaf93e1a4dc39d920ef23971
Lorenzo Pieri <email@lorenzopieri.dev>
2020-02-17 19:39:13 +0100

    Add sentry integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,35 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import 'app.dart';
+import 'sentry.dart';
 
 void main() {
-  runApp(App());
+  FlutterError.onError = onFlutterError;
+
+  // Run the whole app in a zone to capture all uncaught errors.
+  runZoned(() => runApp(App()), onError: onAppError);
 }
+
+var onFlutterError = (FlutterErrorDetails details) {
+  try {
+    onAppError(details.exception, details.stack);
+  } finally {
+    // Also use Flutter's pretty error logging to the device's console.
+    FlutterError.dumpErrorToConsole(details, forceReport: true);
+  }
+};
+
+var onAppError = (Object error, StackTrace stackTrace) {
+  try {
+    sentry.captureException(
+      exception: error,
+      stackTrace: stackTrace,
+    );
+    print('Error sent to sentry.io: $error');
+  } catch (sentryError) {
+    print('Sending report to Sentry failed: $sentryError');
+    print('Original error: $error');
+  }
+};
---
+++ lib/sentry.dart
@@ -0,0 +1,5 @@
+import 'package:sentry/sentry.dart';
+
+final SentryClient sentry = new SentryClient(
+  dsn: "https://bb2ca9444c794202a3559ea4990b1b77@sentry.io/2578903",
+);
