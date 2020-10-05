a51707358d6a14f8210a1506bf90f5b19fa5d353
Lorenzo Pieri <13508373+LRNZ09@users.noreply.github.com>
2020-10-06 00:07:47 +0200

    test: update app goldens test

--- test/flutter_test_config.dart
+++ test/flutter_test_config.dart
@@ -1,9 +1,26 @@
 import 'dart:async';
+import 'dart:convert';
 
-import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void loadFonts() async {
+  final fontManifest = await rootBundle.loadStructuredData<Iterable<dynamic>>(
+    'FontManifest.json',
+    (data) => json.decode(data),
+  );
+
+  for (final Map<String, dynamic> font in fontManifest) {
+    final fontLoader = FontLoader(font['family']);
+    for (final Map<String, dynamic> fontType in font['fonts']) {
+      fontLoader.addFont(rootBundle.load(fontType['asset']));
+    }
+    await fontLoader.load();
+  }
+}
 
 Future<void> main(FutureOr<void> Function() testMain) async {
-  await loadAppFonts();
+  await loadFonts();
 
   return testMain();
 }
