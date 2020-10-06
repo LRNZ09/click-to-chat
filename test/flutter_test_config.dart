ae2149b6dc81ce793dfb5f86079b0dc59230bb19
Lorenzo Pieri <13508373+LRNZ09@users.noreply.github.com>
2020-10-06 17:43:35 +0200

    test: skip app goldens test

--- test/flutter_test_config.dart
+++ test/flutter_test_config.dart
@@ -1,24 +1,7 @@
 import 'dart:async';
-import 'dart:convert';
 
-import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
-
+import 'package:golden_toolkit/golden_toolkit.dart';
 Future<void> main(FutureOr<void> Function() testMain) async {
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  final fontManifest = await rootBundle.loadString(
-    'FontManifest.json',
-  );
-  final fonts = json.decode(fontManifest);
-
-  for (final Map<String, dynamic> font in fonts) {
-    final fontLoader = FontLoader(font['family']);
-    for (final Map<String, dynamic> fontType in font['fonts']) {
-      fontLoader.addFont(rootBundle.load(fontType['asset']));
-    }
-    await fontLoader.load();
-  }
-
+  await loadAppFonts();
   return testMain();
 }
