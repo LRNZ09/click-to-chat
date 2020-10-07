6c619977f0f63d4d84f7ffaec62aaa8fc55ca78d
Lorenzo Pieri <email@lorenzopieri.dev>
2020-10-07 12:50:53 +0200

    fix: enable resampling after run app

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,12 @@ import 'dart:async';
 import 'package:click_to_chat/app.dart';
 import 'package:click_to_chat/debug.dart';
 import 'package:click_to_chat/sentry.dart';
-// import 'package:flutter/gestures.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:in_app_purchase/in_app_purchase.dart';
 import 'package:wakelock/wakelock.dart';
 
 void main() {
-  // GestureBinding.instance.resamplingEnabled = true;
-
   // This captures errors reported by the Flutter framework.
   FlutterError.onError = _onFlutterError;
 
@@ -19,15 +17,20 @@ void main() {
 }
 
 void _runApp() {
-  // For play billing library 2.0 on Android, it is mandatory to call
-  // [enablePendingPurchases](https://developer.android.com/reference/com/android/billingclient/api/BillingClient.Builder.html#enablependingpurchases)
-  // as part of initializing the app.
+  // Inform the plugin that this app supports pending purchases on Android.
+  // An error will occur on Android if you access the plugin `instance`
+  // without this call.
+  //
+  // On iOS this is a no-op.
   InAppPurchaseConnection.enablePendingPurchases();
+
+  runApp(App());
+
+  // Enable smooth scroll on high refresh rate screens
+  GestureBinding.instance.resamplingEnabled = true;
 
   // Keep screen on in debug mode
   Wakelock.toggle(on: isInDebugMode);
-
-  runApp(App());
 }
 
 void _onFlutterError(FlutterErrorDetails details) {
