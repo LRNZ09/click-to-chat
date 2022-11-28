562d5423a82c54faac9288ea7566225e9db34588
Lorenzo Pieri <13508373+LRNZ09@users.noreply.github.com>
2022-11-28 12:04:36 +0100

    chore: remove comments and add const on main

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,5 @@ import 'package:flutter/material.dart';
 import 'app.dart';
 
 void main() {
-  runApp(App());
-
-  // TODO: enable smooth scroll on high refresh rate screens
-  // GestureBinding.instance?.resamplingEnabled = true;
-
-  // TODO: keep screen on in debug mode
-  // Wakelock.toggle(on: isInDebugMode);
+  runApp(const App());
 }
